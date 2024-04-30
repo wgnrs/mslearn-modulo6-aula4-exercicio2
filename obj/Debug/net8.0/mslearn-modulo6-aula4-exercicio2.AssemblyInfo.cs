8373b347a47ec67b2cff51e7cf11c79b1a742c04
Wagner <wagner_silvajr@hotmail.com>
2024-04-30 11:09:50 -0300

    tratado excecoes dentro de process1 e catch de exceções dentro de workflow

--- obj/Debug/net8.0/mslearn-modulo6-aula4-exercicio2.AssemblyInfo.cs
+++ obj/Debug/net8.0/mslearn-modulo6-aula4-exercicio2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mslearn-modulo6-aula4-exercicio2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf54121ad7fc17610a23424a4e223b73af2ddcd2")]
 [assembly: System.Reflection.AssemblyProductAttribute("mslearn-modulo6-aula4-exercicio2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mslearn-modulo6-aula4-exercicio2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
